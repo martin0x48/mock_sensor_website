b5c9b84500de64e5f997dbaa2ae8d2e75fc23e35
martin0x48 <116430952+martin0x48@users.noreply.github.com>
2024-10-27 12:57:53 +0100

    importan changes

--- main.py
+++ main.py
@@ -32,4 +32,4 @@ def index():
     return render_template('index.html', values=values, graph_data=graph_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9100)
+    app.run(host='0.0.0.0', port=6969)
